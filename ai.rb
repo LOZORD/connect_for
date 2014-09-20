aa32fdb5d482500ef44a43ed59c825faeba3d34f
LOZORD <ljrudberg@gmail.com>
2014-09-20 00:35:59 -0500

    Rubocop'd ai.rb

--- ai.rb
+++ ai.rb
@@ -1,23 +1,22 @@
+# This class implements the AI logic for a pseudo-player bot
 class AIPlayer < Player
   def decide_move
     col = defend
     return col unless col.nil?
     col = attack
     return col unless col.nil?
-    col = rand(0...board.width)
-    return col
+    # otherwise, just return a random column
+    rand(0...board.width)
   end
 
   def defend
-  # move across the board, looking for possible wins for other players
-  # TODO
-
-  nil
+    # move across the board, looking for possible wins for other players
+    # TODO
+    nil
   end
 
   def attack
-  
-  # TODO
-  nil
+    # TODO
+    nil
   end
 end
